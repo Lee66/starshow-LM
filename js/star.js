1cc255acdae331da519cb30bbddceda8f938dbcb
robotlee <robotlee8@163.com>
2016-11-28 09:06:58 +0800

    给定数字显示星星

---
+++ js/star.js
@@ -0,0 +1,31 @@
+var AppraiseStar = function(){};
+AppraiseStar.prototype = {
+	show: function(num,ele) {
+		var obj;
+		var id = document.getElementById(ele);
+		var cl = document.getElementsByClassName(ele)[0]
+		if(id!=null){
+			obj=id;
+		}else if(cl!==null){
+			obj=cl;
+		}
+		console.log(num);
+		num == null || num == undefined ? num = 0 : num;
+		var defaultStar = '<i class="iconfont">&#xe630;</i>';
+		var integerStar = '<i class="iconfont starActive">&#xe630;</i>';
+		var halfStarL = '<i class="iconfont icon-pingfenzuo starActive"></i>';
+		var halfStarR = '<i class="iconfont icon-pingfenyou" style="margin-left:-8px;"></i>';
+		var html = '';
+		var len = parseInt(num);
+		for(var i = 1; i <= 5; i++) {
+			if(i <= num) {
+				html += integerStar;
+			} else if((i - 1) == parseInt(num) && (i - 1 < num)) {
+				html += halfStarL + halfStarR;
+			} else if(num + 1 <= i) {
+				html += defaultStar;
+			}
+		}
+		obj.innerHTML = html;
+	}
+};
